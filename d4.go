54cdadebc40ebf89d0176c20845622b79f01d3c8
Benjamin Möckl <oekoben@vollbio.de>
2025-12-05 23:27:07 +0100

    fixed d4 pt 1 (did break while implementing pt2)

--- d4.go
+++ d4.go
@@ -6,21 +6,51 @@ func d4() {
 	yBound := len(input)
 	xBound := len(input[0])
 	grid := make([][]int, 0)
+	grid3 := make([][]int, 0)
 	for range yBound {
 		grid = append(grid, make([]int, xBound))
+		grid3 = append(grid3, make([]int, xBound))
 	}
 
 	for y, line := range input {
 		for x, ele := range line {
 			if ele == '@' {
 				grid[y][x] = -1
+				grid3[y][x] = -1
+			}
+		}
+	}
+
+	cntSt3Nb3 := 0
+	for y, line := range grid3 {
+		for x, ele := range line {
+			if ele != -1 {
+				continue
+			}
+
+			cnt := 0
+			for i := -1; i <= 1; i++ {
+				for j := -1; j <= 1; j++ {
+					checkY, checkX := y+i, x+j
+
+					if checkY < 0 || checkY >= yBound || checkX < 0 || checkX >= xBound || (checkX == x && checkY == y) {
+						continue
+					}
+
+					if grid3[checkY][checkX] == -1 {
+						cnt++
+					}
+				}
+			}
+
+			if cnt < 4 {
+				cntSt3Nb3++
 			}
 		}
 	}
 
 	iter := 1
 	cntSt3NbAll := 0
-	firstSt3Nb := 0
 	for {
 		cntSt3Nb := 0
 		for y, line := range grid {
@@ -50,9 +80,6 @@ func d4() {
 				}
 			}
 		}
-		if firstSt3Nb == 0 {
-			firstSt3Nb = cntSt3Nb
-		}
 		if cntSt3Nb == 0 {
 			break
 		}
@@ -61,6 +88,6 @@ func d4() {
 	}
 
 	p(d, iter)
-	p(r, firstSt3Nb)
+	p(r, cntSt3Nb3)
 	p(r, cntSt3NbAll)
 }
